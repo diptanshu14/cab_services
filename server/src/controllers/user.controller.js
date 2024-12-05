45136be76c60b0500b97e141cc5519d134f55e8a
diptanshu14 <chattopadhyaydiptanshu@gmail.com>
2024-12-05 20:45:13 +0530

    user authentication

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -9,15 +9,21 @@ export const register = async (req, res) => {
 
     const { firstname, lastname, email, password } = req.body
 
-    const result = await userService.register(firstname, lastname, email, password)
-    const { token, user } = result
+    try {
+        const result = await userService.register(firstname, lastname, email, password)
+        const { token, user } = result
 
-    if (!user) {
-        return res.status(400).json({ message: 'User already exists' })
+        if (!user) {
+            return res.status(400).json({ message: 'User already exists' })
+        }
+
+        return res.status(201).json({ token, user })
+    } catch (error) {
+        console.log("Error in register user controller: ", error)
+        res.status(500).json({ message: 'Internal Server Error' })
     }
-
-    return res.status(201).json({ token, user })
 }
+
 
 export const login = async (req, res) => {
     const errors = validationResult(req)
@@ -27,13 +33,30 @@ export const login = async (req, res) => {
 
     const {  email, password } = req.body
 
-    const result = await userService.login(email, password)
-    const { token, user } = result
+    try {
+        const result = await userService.login(email, password)
+        const { token, user } = result
 
-    if (!user) {
-        return res.status(400).json({ message: 'Invalid Credentials' })
+        if (!user) {
+            return res.status(400).json({ message: 'Invalid Credentials' })
+        }
+
+        res.cookie('token', token)
+        res.status(200).json({ token, user })
+    } catch (error) {
+        console.log("Error in login user controller: ", error)
+        res.status(500).json({ message: 'Internal Server Error' })
     }
+}
 
-    res.cookie('token', token)
-    res.status(200).json({ token, user })
+
+export const getUserProfile = async (req, res) => {
+    return res.status(200).json(req.user)
+}
+
+
+export const logoutUser = async (req, res) => {
+    res.clearCookie('token')
+
+    return res.status(200).json({ message: "User logged out" })
 }
--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import express from "express"
 import { body } from "express-validator"
 import * as userCtrl from "../controllers/user.controller.js"
+import { authUser } from "../middlewares/auth.middleware.js"
 
 const router = express.Router()
 
@@ -13,5 +14,9 @@ router.post("/login", [
     body('email').isEmail().withMessage("Invalid Email"),
     body('password').isLength({ min: 8 }).withMessage("Password must be 8 characters long")
 ], userCtrl.login)
+
+router.get("/profile", authUser, userCtrl.getUserProfile)
+
+router.get("/logout", authUser, userCtrl.logoutUser)
 
 export default router
